12177840b9f969c11cfcf8e3780dd21b007a5371
Thom Mahoney <mahoneyt@gmail.com>
2023-02-08 15:52:28 -0600

    can't use jquery

--- github-dismiss-merge-queue-modal.js
+++ github-dismiss-merge-queue-modal.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub Dismiss Merge Queue Modal
 // @namespace    https://thom.is/
-// @version      0.1
+// @version      0.2
 // @description  Dismisses the dialog promoting the merge queue feature for every single repository.
 // @author       Thom Mahoney
 // @match        https://github.com/*
@@ -10,7 +10,7 @@
 (function() {
     'use strict';
 
-    var dismissButton = $(".try-merge-queue-popover form.js-notice-dismiss button[type=submit]");
+    var dismissButton = document.querySelector(".try-merge-queue-popover form.js-notice-dismiss button[type=submit]");
     if (dismissButton) {
         dismissButton.click();
     }
